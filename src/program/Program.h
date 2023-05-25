52ae322de01c650b435322ef7c3826882c468bd3
Monteszenet <monteszenet@gmail.com>
2023-05-25 13:35:26 -0500

    test

--- src/program/Program.h
+++ src/program/Program.h
@@ -23,28 +23,4 @@ private:
 	std::unordered_map<std::string, MMusic*> audios;
 
 	FONT_ID BodoniModa = -1;
-
-	int state = -1;
-	bool state_event = false;
-
-	int cooldown_max = 200;
-	int cooldown = cooldown_max;
 };
-
-//class Program
-//{
-//public:
-//	void Init();
-//	void OnUpdate();
-//	void Finalise();
-//
-//private:
-//	void input();
-//
-//private:
-//	std::unordered_map<std::string, MTexture*> images;
-//	std::unordered_map<std::string, MTexture*> texts;
-//	std::unordered_map<std::string, MMusic*> audios;
-//
-//	FONT_ID BodoniModa = -1;
-//};
